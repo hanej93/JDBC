16f2211b5c94a3192335c4b4a61fb8adcbd2e9f7
hanej93 <hanej93@naver.com>
2021-10-01 15:51:28 +0900

    이전, 다음페이지 구현(1)

--- src/ex1/Program5.java
+++ src/ex1/Program5.java
@@ -7,7 +7,7 @@ import com.newlecture.app.console.NoticeConsole;
 public class Program5 {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		NoticeConsole console = new NoticeConsole();
-
+		//int page;
 		EXIT: 
 		while (true) {
 			console.printNoticeList();
@@ -17,8 +17,12 @@ public class Program5 {
 			case 1: // 상세조회
 				break;
 			case 2: // 이전
+				console.movePrevList();
+				//page--;
 				break;
 			case 3: // 다음
+				console.moveNextList();
+				//page++;
 				break;
 			case 4: // 글쓰기
 				break;
